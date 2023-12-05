060885c3c6343dc7c8721b8c493b4022f30c40b7
Maverick Vitto <mavvitto@yahoo.com>
2023-12-04 17:12:37 -0700

    phase 1 sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+function submitData(name, email) {   
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    }
+    return fetch('http://localhost:3000/users', configurationObject)    
+    
+    .then(function (response) {
+    return response.json()
+    })
+    
+    .then(data => {
+        const newID = data.id
+        const idElement = document.createElement("p")
+        idElement.textContent = `${newID}`
+        document.body.appendChild(idElement)
+    })
+    
+    .catch(error => {
+    const errorMessage = error.message;
+    const errorElement = document.createElement("p");
+    errorElement.textContent = `Error: ${errorMessage}`;
+    document.body.appendChild(errorElement);
+    })
+}
+    
+    
+
+
